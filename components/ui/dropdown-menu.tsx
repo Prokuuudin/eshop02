bfbc2e942aca8097ceba15b41938262228da24da
Aleksandr Prokudin <prokuuudin@gmail.com>
2026-06-04 10:34:09 +0300

    fix: compact dropdown menu items (py-1.5 -> py-1)

--- components/ui/dropdown-menu.tsx
+++ components/ui/dropdown-menu.tsx
@@ -32,7 +32,7 @@ const DropdownMenuItem = React.forwardRef<
   <DropdownMenuPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-gray-100 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-gray-800',
+      'relative flex cursor-pointer select-none items-center rounded-sm px-2 py-1 text-sm outline-none transition-colors focus:bg-gray-100 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 dark:focus:bg-gray-800',
       className
     )}
     {...props}
